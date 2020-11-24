c36cf60c7aa76104f4c5f1966df38f842c88d721
MiekeH <63008309+MiekeH@users.noreply.github.com>
2020-11-24 15:21:29 +0100

    Add files via upload
    New updated HTML included two external files

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello JavaScript!";
+  
+  document.getElementById("demo1").style-backgroundColor = "yellow";
+  document.getElementById("demo2").innerHTML = "Hello Reader of my Career path!";
+  document.getElementById("demo3").innerHTML = Date();
+}
+
+</script> 
