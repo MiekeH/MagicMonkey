ab4325e8c1e1b4edd39f422bcab92f0ce70dd433
MiekeH <63008309+MiekeH@users.noreply.github.com>
2020-11-27 11:20:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-<script>
 function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello JavaScript!";
-  
-  document.getElementById("demo1").style-backgroundColor = "yellow";
-  document.getElementById("demo2").innerHTML = "Hello Reader of my Career path!";
   document.getElementById("demo3").innerHTML = Date();
 }
-
-</script> 
